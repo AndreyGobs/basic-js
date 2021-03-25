1970947ba1af28b299d17821b868160d3aee6c88
AndreyGobs <55401672+AndreyGobs@users.noreply.github.com>
2021-03-26 02:08:54 +0300

    feat: add transform-massive func

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,57 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (typeof arr !== 'object') {
+
+    throw new CustomError('Not implemented');
+  }
+
+
+  let transformArr = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+  
+    if (arr[i] == '--double-next') {
+      
+      if (arr.indexOf('--double-next') == arr.length - 1) {
+        continue;
+    }
+
+    transformArr.push(arr[i+1]);
+
+  } else if (arr[i] == '--double-prev') {
+
+    if (arr.indexOf('--double-prev') == 0) {
+        continue;
+    }
+
+    transformArr.push(arr[i-1]);
+
+    if (transformArr.indexOf(transformArr.length - 1) == transformArr.indexOf(transformArr.length - 2)) {
+        continue;
+    }
+
+    transformArr.push(arr[i-1]);
+
+  } else if (arr[i] == '--discard-prev') {
+
+    transformArr.pop();
+
+  } else if (arr[i] == '--discard-next') {
+
+    if (arr.indexOf('--discars-next') == arr.length - 1) {
+        return;
+    }
+
+      i = i + 2;
+
+  } else {
+
+      transformArr.push(arr[i])
+
+  }
+
+}
+
+return transformArr;
 };

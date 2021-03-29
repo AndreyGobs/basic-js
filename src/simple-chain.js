d4aa8a22efcfaaf2c0881a1bf86cd66b215d952b
AndreyGobs <55401672+AndreyGobs@users.noreply.github.com>
2021-03-30 00:48:08 +0300

    feat: add simple-chain func

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chainLinks : [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+    return this.chainLinks.length;
+
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+    this.chainLinks.push(String(value));
+    return this;
+
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+    if (typeof position !== 'number') {
+      this.chainLinks = [];
+      throw new CustomError('Position is not a number');
+    }
+
+    if ((position - 1) > this.chainLinks.length || position < 0 || !((position ^ 0) === position)) {
+      this.chainLinks = [];
+      throw new CustomError('Position is invalid');
+    }
+    this.chainLinks.splice((position - 1) , 1);
+    
+    return this;
+
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+    this.chainLinks.reverse();
+    return this;
+
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+    let result = this.chainLinks.map(element => `( ${element} )`).join("~~");
+    this.chainLinks = [];
+    return result;
   }
 };
 
